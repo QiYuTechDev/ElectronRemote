b57f77157821bba7777f151514194a61ed8316ea
奇遇科技 <69790869+QiYuTechDev@users.noreply.github.com>
2021-06-03 14:51:30 +0000

    fix @electron/remote not working
    NodePolyfillePlugin rewrite `process` variable

--- demo/webpack.renderer.config.js
+++ demo/webpack.renderer.config.js
@@ -18,7 +18,7 @@ module.exports = {
     module:  {
         rules,
     },
-    plugins: [...plugins, new NodePolyfillPlugin()],
+    plugins: [...plugins, new NodePolyfillPlugin({excludeAliases: ['process']})],
     resolve: {
         extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
     },
